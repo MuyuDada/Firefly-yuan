267ea165f3e5742b0938df28e72457af34e1a63f
MuyuDada <2756390658@qq.com>
2026-08-26 19:10:49 +0800

    添加恋爱计时

--- src/config/relationshipConfig.ts
+++ src/config/relationshipConfig.ts
@@ -4,8 +4,8 @@ export const relationshipConfig: RelationshipConfig = {
 	// 恋爱开始日期
 	startDate: "2026-07-03",
 	// 双方名称
-	name1: "---------WHB",
-	name2: "XH----------",
+	name1: "----------XH",
+	name2: "WHB---------",
 	// 双方头像
 	avatar1: "https://i.imgs.ovh/2026/08/26/c71f994485c2d80f608d6a9bd60bd1ae.jpg",
 	avatar2: "https://i.imgs.ovh/2026/08/26/bd2b41f2cd2c8a2ecbd7a9f5e3560fb9.jpg",
