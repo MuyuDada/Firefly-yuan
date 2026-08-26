701f5e60d671b27f786d995bfdebf5793feb3fd7
MuyuDada <2756390658@qq.com>
2026-08-26 19:07:58 +0800

    添加恋爱组件

--- src/config/relationshipConfig.ts
+++ src/config/relationshipConfig.ts
@@ -7,8 +7,8 @@ export const relationshipConfig: RelationshipConfig = {
 	name1: "---------WHB",
 	name2: "XH----------",
 	// 双方头像
-	avatar1: "https://i.imgs.ovh/2026/08/26/bd2b41f2cd2c8a2ecbd7a9f5e3560fb9.jpg",
-	avatar2: "https://i.imgs.ovh/2026/08/26/c71f994485c2d80f608d6a9bd60bd1ae.jpg",
+	avatar1: "https://i.imgs.ovh/2026/08/26/c71f994485c2d80f608d6a9bd60bd1ae.jpg",
+	avatar2: "https://i.imgs.ovh/2026/08/26/bd2b41f2cd2c8a2ecbd7a9f5e3560fb9.jpg",
 	// 小组件标题
 	title: "我和宝宝在一起已经",
 };
