0d664578bd88657c4f7ea252c44b9343d956cd06
MuyuDada <2756390658@qq.com>
2026-09-07 13:47:01 +0800

    添加友链

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -29,7 +29,7 @@ export const friendsConfig: FriendLink[] = [
 		desc: "飞萤之火自无梦的长夜亮起，绽放在终竟的明天。",
 		siteurl: "https://blog.cuteleaf.cn",
 		tags: ["Blog"],
-		weight: 10, // 权重，数字越大排序越靠前
+		weight: 3, // 权重，数字越大排序越靠前
 		enabled: true, // 是否启用
 	},
 	{
@@ -38,7 +38,7 @@ export const friendsConfig: FriendLink[] = [
 		desc: "Firefly主题模板文档",
 		siteurl: "https://docs-firefly.cuteleaf.cn",
 		tags: ["Docs"],
-		weight: 9,
+		weight: 2,
 		enabled: true,
 	},
 	{
@@ -47,7 +47,16 @@ export const friendsConfig: FriendLink[] = [
 		desc: "The web framework for content-driven websites. ⭐️ Star to support our work!",
 		siteurl: "https://github.com/withastro/astro",
 		tags: ["Framework"],
-		weight: 8,
+		weight: 1,
+		enabled: true,
+	},
+	{
+		title: "池泛の小窝",
+		imgurl: "https://chortle.asia/uploads/image/avater_1786082652615.jpg",
+		desc: "山水有相逢，来日皆可期",
+		siteurl: "https://chortle.asia",
+		tags: ["Framework"],
+		weight: 4,
 		enabled: true,
 	},
 ];
